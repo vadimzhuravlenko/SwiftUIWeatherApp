5ed6ccae54cea29e87ccd1df3dc026ceeb0fddf8
fnyekes <fnyekes@mail.ru>
2022-04-05 23:21:15 +0300

    Initial Commit

---
+++ SwiftUI weatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI weatherApp
+//
+//  Created by Vadim Zhuravlenko on 5.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI weatherApp/SwiftUI_weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_weatherAppApp.swift
+//  SwiftUI weatherApp
+//
+//  Created by Vadim Zhuravlenko on 5.04.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
